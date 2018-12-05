75efe353b6f5b11943afe56ec2f6835017f6e3cf
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-12-05 10:23:29 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let myText = "Jenny Jäderborn";
+let myArray = myText.split('');
+let timer; 
+
+loopingText = () => {
+
+    if(myArray.length > 0){
+ 
+    document.getElementById('myName').innerHTML += myArray.shift();
+    } else {
+        clearTimeout(timer);
+    }
+
+    timer = setTimeout('loopingText()', 100);
+}
+
+loopingText();
+
+
 const section = document.getElementById('section2');
 
 const h1 = document.createElement('h1');
@@ -11,27 +30,34 @@ fetch('./info.json')
 .then(response => response.json())
 .then(data => {
   info = data;
-  createCards()
+    createCards()
 })
 .catch(error => console.error(error)
 )
 
 
+
+
+
 createCards = () => {
 for(let i = 0; i < info.length; i++){
     let card = document.createElement('div');
+    card.className = `cardRow`;
     card.innerHTML = `
-                     <div>
-                     <h4>${info[i].name}</p>
-                     <img src="${info[i].image}"/>
+                    <div class="infoImage">
+                    <img class='projectImg' src="${info[i].image}"/>
+                    <a href=${info[i].link} target="_blank"><button class="imgBtn">${info[i].buttonText}</button></a>
+                    </div>
+                    <div class="infoBox">
+                     <h4>${info[i].name}</h4>
                      <p>${info[i].text}</p>
                      </div>
                         
-                        
                     `;
     wrapper.appendChild(card)
+    }
 }
-}
+
 
 section.appendChild(h1);
 section.appendChild(wrapper);
@@ -40,5 +66,5 @@ section.appendChild(wrapper);
 
 
 
-//tanke - kanske ska alla apenchild osv komma längst ner i filen. 
+//tanke - kanske ska alla apenchild osv komma längst ner i filen.
 
